9037baecddf05780608c6e842a19fba0d95d6dcb
Patrick Mueller <pmuellr@apache.org>
2015-03-27 00:48:01 -0400

    now does something interesting

---
+++ www/code2icon.js
@@ -0,0 +1,55 @@
+// Licensed under the Apache License. See footer for details.
+
+function code2icon(code) {
+  return Code2Icon[code] || "wi-alien"
+}
+
+var Code2Icon = {
+   4: "wi-thunderstorm",
+   5: "wi-sleet",
+   6: "wi-sleet",
+   7: "wi-sleet",
+   8: "wi-sleet",
+   9: "wi-sprinkle",
+  10: "wi-sleet",
+  11: "wi-rain",
+  12: "wi-rain",
+  13: "wi-snow",
+  14: "wi-snow",
+  15: "wi-snow",
+  16: "wi-snow",
+  17: "wi-hail",
+  18: "wi-sleet",
+  19: "wi-dust",
+  20: "wi-fog",
+  21: "wi-fog",
+  22: "wi-smoke",
+  24: "wi-dust",
+  25: "wi-snow",
+  26: "wi-cloudy",
+  27: "wi-cloudy",
+  28: "wi-cloudy",
+  29: "wi-cloudy",
+  30: "wi-cloudy",
+  31: "wi-day-sunny",
+  32: "wi-day-sunny",
+  33: "wi-day-sunny",
+  34: "wi-day-sunny",
+  38: "wi-lightning",
+  40: "wi-rain",
+  42: "wi-snow",
+  47: "wi-lightning",
+}
+//------------------------------------------------------------------------------
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//------------------------------------------------------------------------------
--- www/index.js
+++ www/index.js
@@ -18,90 +18,110 @@ Locations = getLocations()
 function onLoad() {
   Map = L.map("map")
 
-  var bounds = []
+  // add markers, calculate bounds
   Locations.forEach(function(location){
-    var icon = L.icon({
-      iconUrl:   "icon.png",
-      iconSize:  [32,32],
-      className: "location-icon",
+    getCurrentConditions(location)
+
+    var marker = L.marker(location, {
+      title:   location.name,
+      alt:     location.name,
+      opacity: 0
     })
 
-    var marker = L.marker(location, {icon: icon})
     location.marker = marker
-
     marker.addTo(Map)
-
-    bounds.push([location.lat, location.lon])
   })
 
-  var layer = L.esri.basemapLayer("Streets")
-  layer.addTo(Map)
+  // add layer control
+  var ngLayer = L.esri.basemapLayer("NationalGeographic")
+  ngLayer.addTo(Map)
 
-  Map.fitBounds(bounds, {padding:[20,20]})
+  var baseMaps = {
+    Streets:            L.esri.basemapLayer("Streets"),
+    Topographic:        L.esri.basemapLayer("Topographic"),
+    NationalGeographic: ngLayer,
+    Oceans:             L.esri.basemapLayer("Oceans"),
+    Gray:               L.esri.basemapLayer("Gray"),
+    DarkGray:           L.esri.basemapLayer("DarkGray"),
+    Imagery:            L.esri.basemapLayer("Imagery"),
+    ShadedRelief:       L.esri.basemapLayer("ShadedRelief"),
+  }
 
-  // Map.on("dragend", onViewReset)
-  Map.on("moveend", onViewReset)
-  Map.on("zoomend", onViewReset)
+  L.control.layers(baseMaps).addTo(Map)
+
+  // add info box
+  var info = L.control({position: "bottomleft"})
+
+  info.onAdd = function (map) {
+    var div = L.DomUtil.create("div", "info")
+
+    div.innerHTML = "<h3>Weather Demo</h3>" +
+      "<a href='https://bluemix.net/deploy?repository=https://github.com/IBM-Bluemix/weather-demo.git' target='_blank'>" +
+        "<img src='http://bluemix.net/deploy/button.png' alt='Bluemix button' />" +
+      "</a>"
+
+    return div
+  }
+
+  info.addTo(Map)
+
+  // fit to bounds
+  var bounds = [
+    { lat: 44.32, lon:  -69.76 }, // maine
+    { lat: 38.55, lon: -121.46 }, // california
+  ]
+  Map.fitBounds(bounds, {padding:[0,0]})
 }
 
 //------------------------------------------------------------------------------
-function onViewReset() {
+function getCurrentConditions(location) {
+  var lat = location.lat
+  var lon = location.lon
+
+  $.ajax("/api/currentConditions/" + lat + "," + lon, {
+    dataType: "json",
+    success: function(data, status, jqXhr) {
+      gotCurrentConditions(location, data, status, jqXhr)
+    }
+  })
 }
 
-function getLocations() {
-  return [
-    { lat: 32.36, lon:  -86.27, name: "Montgomery, Alabama" },
-    { lat: 58.30, lon: -134.41, name: "Juneau, Alaska" },
-    { lat: 33.44, lon: -112.07, name: "Phoenix, Arizona" },
-    { lat: 34.73, lon:  -92.33, name: "Little Rock, Arkansas" },
-    { lat: 38.55, lon: -121.46, name: "Sacramento, California" },
-    { lat: 39.73, lon: -104.98, name: "Denver, Colorado" },
-    { lat: 41.76, lon:  -72.67, name: "Hartford, Connecticut" },
-    { lat: 39.16, lon:  -75.52, name: "Dover, Delaware" },
-    { lat: 30.45, lon:  -84.27, name: "Tallahassee, Florida" },
-    { lat: 33.76, lon:  -84.39, name: "Atlanta, Georgia" },
-    { lat: 21.30, lon:  -157.8, name: "Honolulu, Hawaii" },
-    { lat: 43.61, lon:  -116.2, name: "Boise, Idaho" },
-    { lat: 39.78, lon:  -89.65, name: "Springfield, Illinois" },
-    { lat: 39.79, lon:  -86.14, name: "Indianapolis, Indiana" },
-    { lat: 41.59, lon:  -93.62, name: "Des Moines, Iowa" },
-    { lat: 39.04, lon:  -95.69, name: "Topeka, Kansas" },
-    { lat: 38.19, lon:  -84.86, name: "Frankfort, Kentucky" },
-    { lat: 30.45, lon:  -91.14, name: "Baton Rouge, Louisiana" },
-    { lat: 44.32, lon:  -69.76, name: "Augusta, Maine" },
-    { lat: 38.97, lon:  -76.50, name: "Annapolis, Maryland" },
-    { lat: 42.23, lon:  -71.02, name: "Boston, Massachusetts" },
-    { lat: 42.73, lon:  -84.54, name: "Lansing, Michigan" },
-    { lat: 44.95, lon:  -93.09, name: "Saint Paul, Minnesota" },
-    { lat: 32.32, lon:  -90.20, name: "Jackson, Mississippi" },
-    { lat: 38.57, lon:  -92.18, name: "Jefferson City, Missouri" },
-    { lat: 46.59, lon: -112.02, name: "Helana, Montana" },
-    { lat: 40.80, lon:  -96.67, name: "Lincoln, Nebraska" },
-    { lat: 39.16, lon: -119.75, name: "Carson City, Nevada" },
-    { lat: 43.22, lon:  -71.54, name: "Concord, New Hampshire" },
-    { lat: 40.22, lon:  -74.75, name: "Trenton, New Jersey" },
-    { lat: 35.66, lon: -105.96, name: "Santa Fe, New Mexico" },
-    { lat: 42.65, lon:  -73.78, name: "Albany, New York" },
-    { lat: 35.77, lon:  -78.63, name: "Raleigh, North Carolina" },
-    { lat: 48.81, lon: -100.77, name: "Bismarck, North Dakota" },
-    { lat: 39.96, lon:  -83.00, name: "Columbus, Ohio" },
-    { lat: 35.48, lon:  -97.53, name: "Oklahoma City, Oklahoma" },
-    { lat: 44.93, lon: -123.02, name: "Salem, Oregon" },
-    { lat: 40.26, lon:  -76.87, name: "Harrisburg, Pennsylvania" },
-    { lat: 41.82, lon:  -71.42, name: "Providence, Rhode Island" },
-    { lat: 34.00, lon:  -81.03, name: "Columbia, South Carolina" },
-    { lat: 44.36, lon: -100.33, name: "Pierre, South Dakota" },
-    { lat: 36.16, lon:  -86.78, name: "Nashville, Tennessee" },
-    { lat: 30.26, lon:  -97.75, name: "Austin, Texas" },
-    { lat: 40.75, lon: -111.89, name: "Salt Lake City, Utah" },
-    { lat: 44.26, lon:  -72.57, name: "Montpelier, Vermont" },
-    { lat: 37.54, lon:  -77.46, name: "Richmond, Virginia" },
-    { lat: 47.04, lon: -122.89, name: "Olympia, Washington" },
-    { lat: 38.34, lon:  -81.63, name: "Charleston, West Virginia" },
-    { lat: 43.07, lon:  -89.38, name: "Madison, Wisconsin" },
-    { lat: 41.14, lon: -104.80, name: "Cheyenne, Wyoming" }
-  ]
+//------------------------------------------------------------------------------
+function gotCurrentConditions(location, data, status, jqXhr) {
+  var observation = data.observation
+  if (null == observation) return
+
+  var icon_code = observation.icon_code
+  var icon = code2icon(icon_code)
+
+  var desc = observation.phrase_32char
+
+  var temp = ""
+  if (observation.imperial) {
+    temp = observation.imperial.temp + "F - "
+  }
+
+  var icon = L.divIcon({
+    html:      "<i class='wi " + icon + "'></i>",
+    iconSize:  [64,64],
+    className: "location-icon"
+  })
+
+  var popupText = location.name + "<br>" + temp + desc
+
+  var marker = location.marker
+  marker.setIcon(icon)
+  marker.bindPopup(popupText)
+
+  // delay display to let bootstrap do it's thing
+  setTimeout(displayMarker, 1000)
+
+  //-----------------------------------
+  function displayMarker() {
+    marker.setOpacity(1)
+  }
 }
+
 //------------------------------------------------------------------------------
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
