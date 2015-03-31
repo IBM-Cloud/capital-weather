d5c53ed63932d737ecc602901a6330f31599ff4e
Patrick Mueller <pmuellr@apache.org>
2015-03-31 10:19:09 -0400

    add link to press release, bluemix, TWC

--- www/help-text.js
+++ www/help-text.js
@@ -6,14 +6,23 @@ function getHelpHTML() {
 
   html.push("<h3>capital weather</h3>")
 
-  html.push("<p>This Bluemix application uses an upcoming weather service")
-  html.push("to display the weather in")
-  html.push("capitals around the world, and US state capitals.")
-
   html.push("<p>Click on a weather icon to see detailed weather information")
   html.push("about that location.")
   html.push("Double-click elsewhere on the map to see the weather in")
   html.push("that location.")
+
+  html.push("<p>This")
+  html.push("<a target='_blank' href='https://bluemix.net'>Bluemix</a>")
+  html.push("application uses data from")
+  html.push("<a target='_blank' href='http://www.wsi.com/'>The Weather Company</a>")
+  html.push("to display the weather in")
+  html.push("capitals around the world, and US state capitals.")
+
+  html.push("<p>For more information on the partnership between IBM and")
+  html.push("The Weather Company, see the press release")
+  html.push("<a target='_blank' href='http://www-03.ibm.com/press/us/en/pressrelease/46446.wss'>")
+  html.push("IBM and The Weather Company Partner to Bring Advanced Weather Insights to Business")
+  html.push("</a>")
 
   return html.join("\n")
 }
